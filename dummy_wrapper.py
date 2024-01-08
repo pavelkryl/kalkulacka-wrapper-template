892053ef23e80b0a73fedcb4e74e5a33aa11d013
Pavel Kryl <kryl@arcig.cz>
2024-01-08 17:20:38 +0100

    comment

--- dummy_wrapper.py
+++ dummy_wrapper.py
@@ -2,9 +2,14 @@
 from abstract_kalkulator import AbstractKalkulator
 
 
+
 class DummyWrapper(AbstractKalkulator):
+    """
+    Hlupacek, jen deleguje, nic navic neumi.
+    """
 
     def __init__(self, delegate: AbstractKalkulator) -> None:
+        # ulozim si delegata, abych mel na koho "svest vinu"
         self._delegate = delegate
 
     def plus(self, op1: int, op2: int) -> int:
