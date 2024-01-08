404549603b5575cb2579d9d9aa173f965d904a3f
Pavel Kryl <kryl@arcig.cz>
2024-01-08 17:21:26 +0100

    comment

--- kalkulator.py
+++ kalkulator.py
@@ -2,6 +2,9 @@ from abstract_kalkulator import AbstractKalkulator
 
 
 class Kalkulator(AbstractKalkulator):
+    """
+    Ten jediny kdo opravdu pocita.
+    """
 
     def plus(self, op1: int, op2: int) -> int:
         return op1 + op2
